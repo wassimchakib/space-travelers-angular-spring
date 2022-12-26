147df755ace0f6bd8ec54a91f0fe6fbd0e78c57a
Wassim <wassimchakib97@gmail.com>
2022-12-26 14:12:16 +0100

    Add navbar

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { RocketsComponent } from './components/rockets/rockets.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { MissionsComponent } from './components/missions/missions.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RocketsComponent,
+    ProfileComponent,
+    MissionsComponent
   ],
   imports: [
     BrowserModule,
